8b07a128d551b8489388606333c5e663674b8119
HarryXu497 <harryxu497@gmail.com>
2024-07-27 16:46:57 -0400

    fdsgalkfdgnfdg

---
+++ spine-align/src/lib/components/VideoPlayer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  	import authStore from "$lib/stores/authStore";
+
+	authStore.subscribe(async (user) => {
+		if (!user) {
+			return;
+		}
+		
+		const mediaStream = await navigator.mediaDevices.getUserMedia({
+			video: true,
+			audio: true,
+		});
+	})
+</script>
