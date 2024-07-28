12937015919eb6a208334d0109f12ee06cf69d79
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-07-27 23:39:02 -0400

    login pages!

--- front-end/src/routes/sign-up/+page.svelte
+++ front-end/src/routes/sign-up/+page.svelte
@@ -3,4 +3,20 @@
 
 </script>
 
-<AuthForm formType={"sign-up"}/>
+<svelte:head>
+	<title>Sign Up | Spine Align</title>
+</svelte:head>
+<div class="w-screen size flex flex-col items-center justify-center">
+	<div class="w-fit">
+		<h1 class="text-3xl mb-4">
+			Sign <span class="text-accent">Up</span>
+		</h1>
+		<AuthForm formType={"sign-up"}/>
+	</div>
+</div>
+
+<style>
+	.size {
+		height: calc(100vh - 5rem);
+	}
+</style>
