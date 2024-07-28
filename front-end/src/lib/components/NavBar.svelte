ba560e0b7529f421a5e2f169f751c54444c60604
Andrew Chu <67875282+andrewchu16@users.noreply.github.com>
2024-07-27 20:39:06 -0400

    navbar fixed styling

--- front-end/src/lib/components/NavBar.svelte
+++ front-end/src/lib/components/NavBar.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
+  import { base } from "$app/paths";
 </script>
 
-<nav class="w-full bg-white flex flex-row justify-between">
+<nav class="w-full flex flex-row h-20 justify-between px-8">
   <!-- Logo -->
-  <h1 class="flex items-center text-[36px]">
-    <Icon icon="ri:sparkling-fill" class="inline-block mr-[36px]" />
+  <h1 class="flex items-center text-2xl text-black">
+    <Icon icon="ri:sparkling-fill" class="inline-block mr-1" />
     spine<span class="text-accent">align</span>
   </h1>
-  <div class="flex justify-between bg-light-gray gap-4 text-[36px]">
-    <a class="rounded-lg bg-accent text-white">
+  <div class="flex justify-between gap-4 text-xl items-center">
+    <a class="rounded-xl bg-accent text-white px-3 py-2 drop-shadow-xl" href="{base}/sign-up">
         Sign up
     </a>
-    <a class="rounded-lg">
+    <a class="rounded-xl text-black" href="{base}/log-in">
         Log in
     </a>
   </div>
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     colors: {
       "accent": "#4744d9",
       "light-accent": "#D5D4F9",
-      "black": "#000000",
+      "black": "#333",
       "white": "#F5F5F5",
       "background": "#EDECF4",
       "dark-gray": "#999999",
